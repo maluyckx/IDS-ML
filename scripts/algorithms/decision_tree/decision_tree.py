f507c6f7feea40c784872bb9e6c792e67b731a81
ayoub <ayoubbouh03@gmail.com>
2023-10-19 15:22:32 +0200

    [UPDATE] update features.py file

--- scripts/algorithms/decision_tree/decision_tree.py
+++ scripts/algorithms/decision_tree/decision_tree.py
@@ -8,7 +8,7 @@ Authors :
 
 
 from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 
 import pandas as pd
@@ -19,8 +19,7 @@ sys.path.append("../../")
 import constants
 import parsing_dns_trace as parser
 
-# import for testing
-import time
+import features, pickle
 
 def parsing_file(traces):
     """Pair DNS traces based on their IDs using the adjusted parser."""
@@ -65,113 +64,11 @@ def parse_training_data(path_to_bots_tcpdump, path_to_webclients_tcpdump):
 
     return bots_data, webclients_data
 
-
-def aggregate_data(data):
-    # Aggregate data
-    # {18712: {'request': {'timestamp': '13:22:44.546969', 'host': 'unamur021', 'query_type': 'A', 'domain': 'kumparan.com', 'length': 30, 'responses': [], 'counts': None, 'request_id': 18712}, 'response': {'timestamp': '13:22:44.580851', 'host': 'one', 'query_type': 'A', 'domain': None, 'length': 62, 'responses': [('A', '104.18.130.231'), ('A', '104.18.129.231')], 'counts': (2, 0, 0), 'request_id': 18712}}}
-
-    # TODO: double check -> no responses
-
-    request = data['request']
-    response = data['response']
-
-    print(data)
-    print(request)
-    print(response)
-
-    timestamp = request['timestamp']
-    host = request['host']
-    query_type = request['query_type']
-    domain = request['domain']
-    length_request = request['length']
-
-    if len(response) == 0:
-        length_response = 0
-        responses = ()
-        counts = None
-    else:
-        length_response = response['length']
-        responses = response['responses']
-        counts = response['counts']
-
-    return {"timestamp": timestamp, "host": host, "query_type": query_type, "domain": domain, "length_request": length_request,"length_response": length_response,"responses": responses,"counts": counts}
-
-
-def convert_to_dataframe(bots_data, webclients_data):
-    # Convert to DataFrame
-
-    bots = []
-
-    for key in bots_data.keys():
-        bots.append(aggregate_data(bots_data[key]))
-
-    webclients = []
-    
-    for key in webclients_data.keys():
-        webclients.append(aggregate_data(webclients_data[key]))
-
-    bots_df = pd.DataFrame(bots)
-    bots_df['label'] = 'bot'
-
-    print(bots_df)
-
-    webclients_df = pd.DataFrame(webclients)
-    webclients_df['label'] = 'human'
-
-    # Combine the two datasets
-    combined_df = pd.concat([bots_df, webclients_df], ignore_index=True)
-
-    # Convert categorical features to numerical
-    combined_df['query_type'] = combined_df['query_type'].astype('category')
-    combined_df['domain'] = combined_df['domain'].astype('category')
-    combined_df['host'] = combined_df['host'].astype('category')
-    combined_df['timestamp'] = combined_df['timestamp'].astype('category')
-    combined_df['length_request'] = combined_df['length_request'].astype('int64')
-    combined_df['length_response'] = combined_df['length_response'].astype('int64')
-    combined_df['responses'] = combined_df['responses'].astype('category')
-    combined_df['counts'] = combined_df['counts'].astype('category')
-    
-    return combined_df
-
-
-def encoding_features(combined_df):
-    # Label encode categorical features
-    label_encoder_query_type = LabelEncoder()
-    combined_df['query_type_encoded'] = label_encoder_query_type.fit_transform(
-        combined_df['query_type'].astype(str))
-
-    label_encoder_domain = LabelEncoder()
-    combined_df['domain_encoded'] = label_encoder_domain.fit_transform(
-        combined_df['domain'])
-
-    label_encoder_host = LabelEncoder()
-    combined_df['host_encoded'] = label_encoder_host.fit_transform(
-        combined_df['host'])
-
-    label_encoder_timestamp = LabelEncoder()
-    combined_df['timestamp_encoded'] = label_encoder_timestamp.fit_transform(
-        combined_df['timestamp'])
-
-    label_encoder_length_request = LabelEncoder()
-    combined_df['length_request_encoded'] = label_encoder_length_request.fit_transform(
-        combined_df['length_request'])
-
-    label_encoder_length_response = LabelEncoder()
-    combined_df['length_response_encoded'] = label_encoder_length_response.fit_transform(
-        combined_df['length_response'])
-    
-    label_encoder_responses = LabelEncoder()
-    combined_df['responses_encoded'] = label_encoder_responses.fit_transform(
-        combined_df['responses'])
-
-    return combined_df
-
-
 def train_decision_tree(combined_df):
     print("####\nTraining the decision tree classifier...")
     # Split the data into training and testing sets
     list_of_features = ['query_type_encoded',
-                        'domain_encoded', 'host_encoded', 'timestamp_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded']
+                        'domain_encoded', 'host_encoded', 'timestamp_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded', 'counts_encoded']
     
     X = combined_df[list_of_features]
     y = combined_df['label']
@@ -197,7 +94,7 @@ def testing_eval(path_to_eval_tcpdump1, path_to_eval_tcpdump2):
     combined_df = features.encoding_features(combined_df)
 
     list_of_features = ['query_type_encoded',
-                        'domain_encoded', 'host_encoded', 'timestamp_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded']
+                        'domain_encoded', 'host_encoded', 'timestamp_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded', 'counts_encoded']
 
     X_test = combined_df[list_of_features]
     y_test = combined_df['label']

ed652c115f9fdccef31b8707719d6f36c8f9e473
ayoub <ayoubbouh03@gmail.com>
2023-10-19 19:01:52 +0200

    [REFACTOR] Refactoring the code

--- scripts/algorithms/decision_tree/decision_tree.py
+++ scripts/algorithms/decision_tree/decision_tree.py
@@ -6,42 +6,36 @@ Authors :
     - BOUHNINE Ayoub 500048
 """
 
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+# ML dependencies
 from sklearn.tree import DecisionTreeClassifier
+import pickle
 
-import pandas as pd
-import re
-
+# Personal dependencies
 import sys
 sys.path.append("../../")
 import constants
 import parsing_dns_trace as parser
+import features
 
-import features, pickle
-
-def train_decision_tree(combined_df):
+def train_decision_tree(bots_data, webclients_data):
     print("####\nTraining the decision tree classifier...")
-    # Split the data into training and testing sets
-    list_of_features = ['query_type_encoded',
-                        'domain_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded', 'counts_encoded']
-    
-    X = combined_df[list_of_features]
-    y = combined_df['label']
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
+    combined_df = features.convert_to_dataframe(bots_data, webclients_data)
+
+    combined_df = features.encoding_features(combined_df)
+
+    X_train = combined_df[constants.LIST_OF_FEATURES]
+    y_train = combined_df['label']
 
     # Train the decision tree classifier
     clf = DecisionTreeClassifier()
     clf.fit(X_train, y_train)
-    # clf.fit(X, y)
+    
     print("Decision tree classifier trained successfully!\n####\n")
-    return clf, X_test, y_test
+    return clf
 
 
-def testing_eval(path_to_eval_tcpdump1, path_to_eval_tcpdump2):
+def eval_decision_tree(path_to_eval_tcpdump1, path_to_eval_tcpdump2):
     print("####\nTesting the decision tree classifier...")
     eval_data1, eval_data2 = parser.parse_training_data(
         path_to_eval_tcpdump1, path_to_eval_tcpdump2)
@@ -50,10 +44,7 @@ def testing_eval(path_to_eval_tcpdump1, path_to_eval_tcpdump2):
 
     combined_df = features.encoding_features(combined_df)
 
-    list_of_features = ['query_type_encoded',
-                        'domain_encoded', 'length_request_encoded', 'length_response_encoded', 'responses_encoded', 'counts_encoded']
-
-    X_test = combined_df[list_of_features]
+    X_test = combined_df[constants.LIST_OF_FEATURES]
     y_test = combined_df['label']
 
     print("Decision tree classifier tested successfully!\n####\n")
@@ -75,34 +66,21 @@ def diagram_decision_tree(clf):
 
 def main_decision_tree():
     # Use a first dataset to train the classifier
-    bots_data, webclients_data = parser.parse_training_data(
-        constants.PATH_TO_BOTS_TCPDUMP, constants.PATH_TO_WEBCLIENTS_TCPDUMP)
+    bots_data, webclients_data = parser.parse_training_data(constants.PATH_TO_BOTS_TCPDUMP, constants.PATH_TO_WEBCLIENTS_TCPDUMP)
 
-    combined_df = features.convert_to_dataframe(bots_data, webclients_data)
+    # Training the classifier
+    clf = train_decision_tree(bots_data, webclients_data)
 
-    combined_df = features.encoding_features(combined_df)
-
-    clf, X_test, y_test = train_decision_tree(combined_df)
-
-    # Using another dataset to test the classifier
-    X_test, y_test = testing_eval(
+    # Use a second dataset to test the classifier
+    X_test, y_test = eval_decision_tree(
         constants.PATH_TO_EVAL_TCPDUMP1, constants.PATH_TO_EVAL_TCPDUMP2)
 
-    save_decision_tree(clf)
-    diagram_decision_tree(clf)
     # Test the classifier's accuracy on the test set
     accuracy = clf.score(X_test, y_test)
     print("Accuracy of the model : ", accuracy)
 
-    # Predict the labels of new and unseen data from X_test and y_test
-    y_pred = clf.predict(X_test)
-    # print("Prediction :", y_pred.tolist())
-    # print("Prediction :", y_test.tolist())
-
-    # # Count the number of 'human' and the number of 'bot' predictions
-    # print("Number of 'human' predictions :", y_test.tolist().count('human'))
-    # print("Number of 'bot' predictions :", y_test.tolist().count('bot'))
-
-
+    diagram_decision_tree(clf)
+    save_decision_tree(clf)
+    
 if __name__ == "__main__":
     main_decision_tree()

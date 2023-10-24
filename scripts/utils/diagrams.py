632dfb8ca10414bff6e4698621a4bc164532cf68
maluyckx <Marco.Luyckx@ulb.ac.be>
2023-10-24 21:18:00 +0200

    [ADD] diagrams.py

---
+++ scripts/utils/diagrams.py
@@ -0,0 +1,56 @@
+"""
+Goal of the script : Using a decision tree classifier to detect bots in a network traffic trace 
+
+Authors : 
+    - LUYCKX Marco 496283
+    - BOUHNINE Ayoub 500048
+"""
+
+import matplotlib.pyplot as plt
+
+
+def diagram_decision_tree(clf):
+    pass
+
+def diagram_logistic_regression():
+    pass
+
+def diagram_knn():
+    pass
+
+def diagram_random_forest():
+    pass
+
+def diagram_neural_networks():
+    pass
+
+
+
+
+
+def main_diagrams():
+    # Decision Tree
+    diagram_decision_tree()
+    
+    # Logistic Regression
+    diagram_logistic_regression()
+    
+    # KNN
+    diagram_knn()
+    
+    # Random Forest
+    diagram_random_forest()
+    
+    # Neural Networks
+    diagram_neural_networks()
+    
+    
+    
+if __name__ == "__main__":
+    main_diagrams()
+
+
+
+
+
+
